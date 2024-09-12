62d633597a5fc66d0b361daf341bdc6c2c96c863
Tadas Dukavicius <tadas.dukavicius@gmail.com>
2024-09-12 20:20:35 +0300

    Excel file upload

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
-// Import the express and fetch libraries
 const express = require("express");
 const fetch = require("node-fetch");
 const dotenv = require("dotenv");
+const cors = require("cors");
+const path = require("path");
+const multer = require("multer");
 dotenv.config();
 
-// Create a new express application
 const app = express();
 
-// Send a JSON response to a default get request
+app.use(logger);
+app.use(cors());
+
+// Configure multer for file storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // Directory to save the uploaded files
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); // Save with unique filename
+  },
+});
+
+const upload = multer({ storage: storage });
+
 app.get("/ping", async (req, res) => {
   const requestOptions = {
     method: "GET",
@@ -25,32 +40,39 @@ app.get("/ping", async (req, res) => {
     const data = await response.json();
     res.send(data);
   } else {
-    res.send("oops");
+    res.send("Couldn't get the Shop ID");
   }
 });
 
-// app.get("/callback", async (req, res) => {
-//   const requestOptions = {
-//     method: "GET",
-//     headers: {
-//       "x-api-key": process.env.api_key,
-//     },
-//   };
+// POST endpoint to handle file uploads
+app.post("/uploadFile", upload.single("file"), (req, res) => {
+  console.log(req.file);
+  try {
+    // req.file contains the information about the uploaded file
+    const file = req.file;
 
-//   const response = await fetch(
-//     "https://api.etsy.com/v3/application/openapi-ping",
-//     requestOptions
-//   );
+    if (!file) {
+      return res.status(400).send("No file uploaded.");
+    }
 
-//   if (response.ok) {
-//     const data = await response.json();
-//     res.send(data);
-//   } else {
-//     res.send("oops");
-//   }
-// });
+    // Send success response
+    res.json({
+      message: "File uploaded successfully!",
+      fileName: file.filename,
+      originalName: file.originalname,
+      path: file.path,
+    });
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    res.status(500).send("Error uploading file.");
+  }
+});
 
-// Start the server on port 3003
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
+
 const port = 3003;
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
